9b6d819c1036d9cd2c749d1f1ed780bc4a338e8b
Ben Morrison <benmorrison0@gmail.com>
2017-06-04 18:45:00 -0400

    Checkpoint 9 events: completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,17 +1,31 @@
-var animatePoints = function() {
-    var points = document.getElementsByClassName('point');
+var pointsArray = document.getElementsByClassName('point');
 
+var animatePoints = function(points) {
     //Assignment 8 revealPoints function:
-    var revealPoints = function(pointIndex) {
+    var revealPoint = function(pointIndex) {
         points[pointIndex].style.opacity = 1;
         points[pointIndex].style.transform = "scaleX(1) translateY(0)";
         points[pointIndex].style.msTransform = "scaleX(1) translateY(0)";
         points[pointIndex].style.WebkitTransform = "scaleX(1) translateY(0)";
-    };
+    }
 
     for(var i = 0; i < points.length; i++) {
-        revealPoints(i);
+        revealPoint(i);
     }
 };
 
-animatePoints();
+window.onload = function() {
+    // Automatically animate the points on a tall screen where scrolling can't trigger the animation:
+    if(window.innerHeight > 950) {
+        animatePoints(pointsArray);
+    }
+
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+    window.addEventListener('scroll', function(event) {
+        if(document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+            animatePoints(pointsArray);
+        }
+    });
+};
